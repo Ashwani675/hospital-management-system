cecde24c47d7c3a1d947c73614ded8e8f44d8c41
udit tyagi <udittyagi455@gmail.com>
2022-04-10 16:51:21 +0530

    changesss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './component/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <h2>Hello World</h2>
+      <Header></Header>
     </div>
   );
 }
